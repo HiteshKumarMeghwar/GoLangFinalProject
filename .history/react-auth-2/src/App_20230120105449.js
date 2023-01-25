110de728852710abe1434c163ef1d2afd3d96c98
Hitesh Kumar <hiteshkumarkunri@gmail.com>
2023-01-25 17:03:52 +0500

    updated

--- .history/react-auth-2/src/App_20230120105449.js
+++ .history/react-auth-2/src/App_20230120105449.js
@@ -6,7 +6,6 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import PersonalBlog from './pages/PersonalBlog';
-import AllUsers from './pages/AllUsers';
 import CreateBlog from './pages/CreateBlog';
 import BlogDetail from './pages/BlogDetail';
 import EditPost from './pages/EditPost';
@@ -22,7 +21,6 @@ function App() {
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
             <Route path='/personal_posts' element={<PersonalBlog />} />
-            <Route path='/all_users' element={<AllUsers />} />
             <Route path='/allpost/:id' element={<BlogDetail />} />
             <Route path='/create_post' element={<CreateBlog />} />
             <Route path='/edit_post/:id' element={<EditPost />} />
--- .history/react-auth-2/src/components/Nav_20230125165939.jsx
+++ .history/react-auth-2/src/components/Nav_20230125165939.jsx
@@ -56,9 +56,6 @@ function Nav() {
                       <Link to="/personal_posts" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Personal Posts</Link>
                     </li>
                     <li>
-                      <Link to="/all_users" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">All Users</Link>
-                    </li>
-                    <li>
                       <button onClick={logOut} className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
                         Log Out
                       </button>
