aa6c1a46e51474248136bdc0c5e56bcd94879155
Hitesh Kumar <hiteshkumarkunri@gmail.com>
2023-02-20 13:33:43 +0500

    updated

--- .history/react-auth-2/src/pages/CreateBlog_20230124162021.jsx
+++ .history/react-auth-2/src/pages/CreateBlog_20230124162021.jsx
@@ -1,16 +1,17 @@
-import React, {useState, useEffect} from 'react';
+import React, {useEffect, useState} from "react";
 import {useForm} from 'react-hook-form'
 import axios from 'axios'
-import { useParams, useNavigate } from 'react-router-dom'
+// import {useSnackbar} from 'react-simple-snackbar'
+import { useNavigate } from 'react-router-dom'
 
-function EditPost() {
-    const [singlePost, setSinglePost] = useState();
-    const [loading, setLoading] = useState(false);
-    const [imageUpload, setImageUpload] = useState();
-    const [loadingData, setLoadingData] = useState();
-    const {id} = useParams();
-    const navigate = useNavigate();
+export default function CreateBlog () {
     const [image, setImage] = useState();
+    const [loading, setLoading] = useState(false);
+    const [imageData, setImageData] = useState();
+    const [imageUpload, setImageUpload] = useState();
+    const [userData, setUserData] = useState();
+    const [loadingData, setLoadingData] = useState();
+    const navigate = useNavigate();
     const {
         register,
         handleSubmit,
@@ -19,29 +20,61 @@ function EditPost() {
     } = useForm();
 
     useEffect(() => {
-        const singleBlog = () => {
-            axios.get(`http://127.0.0.1:8080/api/allpost/${id}`, {withCredentials: true})
-            .then(function(response) {
-                // handle access .....
-                setSinglePost(response?.data?.data);
-                console.log(response?.data?.data);
-            }).catch(function(error) {
-                // handle error
-                console.log(error);
-            }).then(function() {
-                //  always executed ....
-            });
-        };
         const User = localStorage.getItem("user");
-        if(!User){
+        const parseUser = JSON.parse(User);
+        setUserData(parseUser);
+        if (!User){
             navigate("/login")
         }
-        singleBlog();
-    }, [navigate, id]);
+    }, [navigate]);
+
+    /* const options = {
+        position: "bottom-right",
+        style: {
+        backgroundColor: "gray",
+        border: "2px solid lightgreen",
+        color: "white",
+        fontFamily: "Menlo, monospace",
+        fontSize: "20px",
+        textAlign: "center",
+        },
+        closeStyle: {
+        color: "lightcoral",
+        fontSize: "16px",
+        },
+    }; */
+    // const [openSnackbar] = useSnackbar(options);
+
+    const onSubmit = (data) => {
+        setLoading(true);
+        const body = {
+            ...data,
+            image: imageData,
+            userid: JSON.stringify(userData.id),
+            // phone: parseInt(data.phone),
+        }
+
+        console.log(body)
+        // console.log(body);
+        // return
+        axios.post(`http://localhost:8080/api/createpost`, body)
+        .then(function(response) {
+            // handle access .....
+            setLoading(false);
+            navigate("/personal_posts");
+        }).catch(function(error) {
+            // handle error
+            setLoading(false);
+        }).then(function() {
+            //  always executed ....
+        });
+    };
 
     const handleImage = (e) => {
         const file = e.target.files[0];
         // const size = file.size / 1024;
+        setImageUpload(file);
+
         // data.append("image", file)
         const reader = new FileReader();
         reader.onloadend = function() {
@@ -53,37 +86,19 @@ function EditPost() {
         }
     };
 
-    const onSubmit = (data) => {
-        setLoading(true);
-        const body = {
-            ...data,
-            image: singlePost ? singlePost?.image : imageUpload,
-        }
-
-        axios.put(`http://127.0.0.1:8080/api/updatepost/${id}`, {...body})
-        .then(function(response) {
-            // handle access .....
-            setLoading(false);
-            navigate("/");
-        }).catch(function(error) {
-            // handle error
-            setLoading(false);
-            console.log(error);
-        });
-    }
-
     const uploadImage = () => {
         let formData = new FormData(); // formData object
-        formData.append("image", image ? image.image : singlePost?.image ); // append the value with key, value pair
-        formData.append("name", image ? image.image : singlePost?.image.name);
+        formData.append("image", imageUpload); // append the value with key, value pair
+        formData.append("name", imageUpload.name);
         const config = {
-            headers: {"Content-Type":"multipart/form-data"}
+            headers: {"Content-Type":"multipart/form-data"},
+            withCredentials: true,
         };
         let url = `http://127.0.0.1:8080/api/upload-image/`;
         axios.post(url, formData, config)
         .then((response) => {
             setLoadingData(false);
-            setImageUpload(response?.data?.url);
+            setImageData(response?.data?.url);
             // openSnackbar("Image Uploaded Successfully");
         })
         .catch((error) => {
@@ -97,7 +112,7 @@ function EditPost() {
             <div className="max-w-screen-md mx-auto p-5">
                 <div className="text-center mb-16">
                     <p className="mt-4 text-sm leading-7 text-gray-500 font-regular uppercase">
-                        Edit your Post
+                        Create your Post
                     </p>
                     <h3 className="text-3xl sm:text-4xl leading-normal font-extrabold tracking-right text-gray-900">
                         Express your <span className="text-indigo-600">Feeling</span>
@@ -118,7 +133,6 @@ function EditPost() {
                             {...register("title", {
                                 required: true,
                             })}
-                            defaultValue={singlePost?.title}
                         />
                         {errors.title && errors.title.type === "required" && (
                             <p className='text-red-500 text-xs italic'>
@@ -127,9 +141,9 @@ function EditPost() {
                         )}
                     </div>
                 </div>
-                <div>
-                    <div>
-                    <label title='click to select a picture'>
+                <div className='flex flex-wrap -mx-3 mb-6'>
+                    <div className='w-full md:w-full px-3 mb-6 md:mb-0'>
+                        <label title='click to select a picture'>
                             <input type="file" name="image" id="banner" 
                                 className='hidden'
                                 accept='image/*'
@@ -141,7 +155,7 @@ function EditPost() {
                                 {image ? (
                                     <div className='pt-4'>
                                         <img 
-                                            src={image.image} 
+                                            src={image ? image.image : ""} 
                                             alt="" 
                                             className='object-contain -mt-8 p-5 w-1/2'
                                         />
@@ -149,9 +163,10 @@ function EditPost() {
                                 ) : (
                                     <div className='pt-4'>
                                         <img 
-                                            src={singlePost?.image}
+                                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNaZWu6JFF7vxUdvIhvdG8RLQiMCI0RHUaitDRFpmj&s"
                                             alt="" 
-                                            className='object-contain -mt-8 p-5 w-1/2'
+                                            style={{background: "#EFEFEF"}}
+                                            className="h-full w-48"
                                         />
                                     </div>
                                 )}
@@ -180,7 +195,6 @@ function EditPost() {
                             {...register("desc", {
                                 required: true,
                             })}
-                            defaultValue={singlePost?.desc}
                             rows="10"
                         ></textarea>
                         {errors.desc && errors.desc.type === "required" && (
@@ -196,7 +210,7 @@ function EditPost() {
                             className="shadow text-white px-8 py-3 border border-transparent rounded bg-indigo-600 hover:bg-indigo-400 focus:shadow-outline focus:outline-none"
                             disabled={loading ? true : false}
                         >
-                            {loading ? "Loading ...":"Update Post"}
+                            {loading ? "Loading ...":"Create Post"}
                         </button>
                     </div>
                 </div>
@@ -204,5 +218,3 @@ function EditPost() {
         </div>
     )
 }
-
-export default EditPost
--- .history/react-auth-2/src/pages/PersonalBlog_20230124161048.jsx
+++ .history/react-auth-2/src/pages/PersonalBlog_20230124161048.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
 import axios from 'axios'
+// import {useSnackbar} from 'react-simple-snackbar'
 import { useNavigate, Link } from 'react-router-dom'
 
 export default function PersonalBlog() {
@@ -7,6 +8,23 @@ export default function PersonalBlog() {
     const [loading, setLoading] = useState(false);
     const [deleteLoading, setDeleteLoading] = useState(false);
     const navigate = useNavigate();
+
+    /* const options = {
+        position: "bottom-right",
+        style: {
+        backgroundColor: "gray",
+        border: "2px solid lightgreen",
+        color: "white",
+        fontFamily: "Menlo, monospace",
+        fontSize: "20px",
+        textAlign: "center",
+        },
+        closeStyle: {
+        color: "lightcoral",
+        fontSize: "16px",
+        },
+    }; */
+    // const [openSnackbar] = useSnackbar(options);
 
     const uniqueBlog = () => {
         setLoading(true);
@@ -42,10 +60,14 @@ export default function PersonalBlog() {
         axios.delete(`http://127.0.0.1:8080/api/deletepost/${blog.id}`).then(function(response) {
             // handle access .....
             setDeleteLoading(false);
+            // openSnackbar(error?.response?.data?.message);
             uniqueBlog();
         }).catch(function(error) {
             // handle error
             setDeleteLoading(false);
+            // setMessage(error?.response?.data?.message);
+            // openSnackbar(error?.response?.data?.message);
+            // console.log(error?.response?.data?.message);
         }).then(function() {
             //  always executed ....
         });
